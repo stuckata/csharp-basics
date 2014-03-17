990101786adda21233a957d2226928cfc30b70f6
stuckata <stu.kirkov@gmail.com>
2014-03-17 22:37:30 +0200

    problem5

--- 05. FormattingNumbers/05. FormattingNumbers.cs
+++ 05. FormattingNumbers/05. FormattingNumbers.cs
@@ -4,16 +4,16 @@ class FormattingNumbers
 {
     static void Main()
     {
-        Console.WriteLine("Please, enter an integer \"a\" which fulfill following condition 0 <= a <= 500: ");
+        Console.WriteLine("Please, enter an integer a which fulfill following condition 0 <= a <= 500: ");
         int numA = int.Parse(Console.ReadLine());
         if (numA < 0 || numA > 500)
         {
-            Console.WriteLine("\"a\" do not fulfill 0 <= a <= 500!");
+            Console.WriteLine("a should fulfill 0 <= a <= 500!");
             return;
         }
-        Console.WriteLine("Enter two floating-point variable \"b\": ");
+        Console.WriteLine("Enter floating-point variable b: ");
         float numB = float.Parse(Console.ReadLine());
-        Console.WriteLine("And \"c\": ");
+        Console.WriteLine("Enter floating-point variable c: ");
         float numC = float.Parse(Console.ReadLine());
 
         string symbol = "|";
