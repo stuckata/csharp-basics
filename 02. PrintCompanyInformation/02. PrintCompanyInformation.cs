be613da0779729329f24c4a57dae4ae0c1f2f59d
stuckata <stu.kirkov@gmail.com>
2014-03-17 18:11:43 +0200

    problem2

--- 02. PrintCompanyInformation/02. PrintCompanyInformation.cs
+++ 02. PrintCompanyInformation/02. PrintCompanyInformation.cs
@@ -4,11 +4,31 @@ class PrintCompanyInformation
 {
     static void Main()
     {
-        Console.Write("Company Name: ");
+        Console.WriteLine("Company Name: ");
         string compName = Console.ReadLine();
-        Console.Write("Addres: ");
+        Console.WriteLine("Addres: ");
         string address = Console.ReadLine();
-        Console.Write("Company Phone Number: ");
-        int compPhoneNum = int.Parse(Console.ReadLine());
+        Console.WriteLine("Phone Number: ");
+        var telNum = Console.ReadLine();
+        Console.WriteLine("Fax Number: ");
+        var faxNum = Console.ReadLine();
+        Console.WriteLine("Web Site: ");
+        string webSite = Console.ReadLine();
+        Console.WriteLine("Manager First Name: ");
+        string mngrFName = Console.ReadLine();
+        Console.WriteLine("Manager Last Name: ");
+        string mngrLName = Console.ReadLine();
+        Console.WriteLine("Manager Age: ");
+        int mngrAge = int.Parse(Console.ReadLine());
+        Console.WriteLine("Manager Telephone Number: ");
+        var mngrTelNum = Console.ReadLine();
+
+        Console.WriteLine("Company Name: " + compName);
+        Console.WriteLine("Adress: " + address);
+        Console.WriteLine("Phone Number: " + telNum);
+        Console.WriteLine("Fax Number: " + faxNum);
+        Console.WriteLine("Web Site: " + webSite);
+        Console.WriteLine("Manager First, Last Names and Age: " + mngrFName + " " + mngrLName + " " + mngrAge + " years old.");
+        Console.WriteLine("Manager Telephone Number: " + mngrTelNum);
     }
 }
