6c530e3bc4f65b63b9cf08d8c1ff362023656308
stuckata <stu.kirkov@gmail.com>
2014-03-17 15:44:05 +0200

    problem2

---
+++ 02. PrintCompanyInformation/02. PrintCompanyInformation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class PrintCompanyInformation
+{
+    static void Main()
+    {
+        Console.Write("Company Name: ");
+        string compName = Console.ReadLine();
+        Console.Write("Addres: ");
+        string address = Console.ReadLine();
+        Console.Write("Company Phone Number: ");
+        int compPhoneNum = int.Parse(Console.ReadLine());
+    }
+}
---
+++ 03. CirclePerimeterAndArea/03. CirclePerimeterAndArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class CirclePerimeterAndArea
+{
+    static void Main()
+    {
+        Console.WriteLine("To calculate circle perimeter and area, please enter radius \"r\":");
+        double circleR = double.Parse(Console.ReadLine());
+        double circleA = Math.PI * circleR * circleR;
+        double circleP = 2 * Math.PI * circleR;
+        Console.WriteLine("The Circle Radius is: {0:0.00}, Circle Area: {1:0.00} and Circle Perimeter is: {2:0.00}", circleR, circleA, circleP);
+    }
+}
