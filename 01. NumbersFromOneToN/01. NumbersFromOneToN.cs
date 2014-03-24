3cfe3a389f3dc519df638c1c24f20694186c48de
stuckata <stu.kirkov@gmail.com>
2014-03-24 13:51:05 +0200

    problem01solved

---
+++ 01. NumbersFromOneToN/01. NumbersFromOneToN.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class NumbersFromOneToN
+{
+    static void Main()
+    {
+        Console.WriteLine("This program print all the numbers from 1 to n (positive integer number entered from user.)");
+        Console.WriteLine("Please enter positive integer number n: ");
+        Console.Write("n = ");
+
+        string input = Console.ReadLine();
+        int numN = 0;
+        bool isValid = int.TryParse(input, out numN);
+
+        while ((!isValid) || (numN < 0))
+        {
+            Console.WriteLine("The number is invalid! Please enter new one:");
+            Console.Write("n = ");
+            input = Console.ReadLine();
+            isValid = int.TryParse(input, out numN);
+        }
+        
+        for (int i = 1; i <= numN; i++)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
