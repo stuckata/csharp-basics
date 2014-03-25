ebc18625293258b239610efc65e2533663c32320
stuckata <stu.kirkov@gmail.com>
2014-03-25 17:55:50 +0200

    problem07solved

---
+++ 03. MinMaxSumAndAverageOfNNumbers/03. MinMaxSumAndAverageOfNNumbers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class MinMaxSumAndAverageOfNNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine("This program reads from console n integer numbers, and return as result: min, max, sum and avg values.");
+        Console.WriteLine("Enter n where n has to be positive integer number bigger than one: ");
+        Console.Write("n = ");
+
+        string input = Console.ReadLine();
+        int numN = 0;
+        bool isValid = int.TryParse(input, out numN);
+
+
+        while ((!isValid) || (numN < 2)) //check if the input is a valid integer number bigger than 1
+        {
+            Console.WriteLine("Invalid Number! Enter new number n:");
+            Console.Write("n = ");
+            input = Console.ReadLine();
+            isValid = int.TryParse(input, out numN);
+        }
+
+        int[] numArray = new int[numN];
+        int min = int.MaxValue;
+        int max = int.MinValue;
+        double sum = 0;
+        double avg = 0;
+
+        for (int i = 0; i < numN; i++)
+        {
+            Console.WriteLine("Enter {0} number from {1}: ", (i + 1), numN);
+            input = Console.ReadLine();
+            isValid = int.TryParse(input, out numArray[i]);
+
+            while (!isValid) //check if the input is a valid integer number
+            {
+                Console.WriteLine("Invalid Number! Enter new integer number: ");
+                input = Console.ReadLine();
+                isValid = int.TryParse(input, out numArray[i]);
+            }
+        }
+
+        for (int i = 0; i <= numArray.Length - 1; i++)
+        {
+            if (min > numArray[i])
+            {
+                min = numArray[i];
+            }
+            if (max < numArray[i])
+            {
+                max = numArray[i];
+            }
+            sum = (sum + numArray[i]);
+        }
+        avg = (sum / numN);
+
+        Console.WriteLine("min = " + min);
+        Console.WriteLine("max = " + max);
+        Console.WriteLine("sum = {0 : 0.##}", sum);
+        Console.WriteLine("avg = {0 : 0.00}", avg);
+    }
+}
+
---
+++ 06. CalculatingFactorialDivision/06. CalculatingFactorialDivision.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class CalculatingFactorialDivision
+{
+    static void Main()
+    {
+        Console.WriteLine("This program calculate n! / k! for integers n and k (1 < k < n < 100).");
+        Console.WriteLine("Please, enter n and k: ");
+
+        Console.Write("n = ");
+        string inputN = Console.ReadLine();
+        Console.Write("k = ");
+        string inputK = Console.ReadLine();
+
+        int numN = 0;
+        int numK = 0;
+        int factorialN = 1;
+        int factorialK = 1;
+        double calculation = 0;
+        bool isValidN = int.TryParse(inputN, out numN);
+        bool isValidX = int.TryParse(inputK, out numK);
+
+        while ((!isValidN || !isValidX) || (!(1 < numK) || !(numK < numN)) || !(numN < 100))
+        {
+            Console.WriteLine("Invalid Numbers!");
+            Console.Write("n = ");
+            inputN = Console.ReadLine();
+            Console.Write("k = ");
+            inputK = Console.ReadLine();
+            isValidN = int.TryParse(inputN, out numN);
+            isValidX = int.TryParse(inputK, out numK);
+        }
+
+        for (int i = 1; i <= numN; i++)
+        {
+            factorialN *= i;
+            if (i <= numK)
+            {
+                factorialK *= i;
+            }
+        }
+        calculation = factorialN / factorialK;
+        Console.WriteLine("n! / k! = {0 : 0.##}", calculation);
+    }
+}
+
