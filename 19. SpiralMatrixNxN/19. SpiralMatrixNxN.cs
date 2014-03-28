d71dafcb429d2073d0aa5c7e365119d5d9741f2b
stuckata <stu.kirkov@gmail.com>
2014-03-28 14:27:50 +0200

    homeworksolved

---
+++ 19. SpiralMatrixNxN/19. SpiralMatrixNxN.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class SpiralMatrixNxN
+{
+    static void Main()
+    {
+        Console.WriteLine("This program reads number N and print a matrix in format NxN.");
+        Console.WriteLine("Please enter positive integer number N != 0: ");
+        Console.Write("N = ");
+        string input = Console.ReadLine();
+        int numN = 0;
+        bool isValid = int.TryParse(input, out numN);
+
+        while (!isValid || (numN <= 0))
+        {
+            Console.WriteLine("Invalid number N!");
+            Console.WriteLine("Please enter positive integer number N != 0: ");
+            Console.Write("N = ");
+            input = Console.ReadLine();
+            isValid = int.TryParse(input, out numN);
+        }
+
+        int[,] matrixN = new int[numN, numN];
+        int counter = 1;
+
+        for (int loop = 0; loop <= numN / 2 + 1; loop++)
+        {
+            for (int col = loop; col < numN - loop; col++)
+            {
+                matrixN[loop, col] = counter;
+                counter++;
+            }
+            for (int row = loop + 1; row < numN - loop; row++)
+            {
+                matrixN[row, numN - (loop + 1)] = counter;
+                counter++;
+            }
+            for (int col = numN - (loop + 2); col >= loop; col--)
+            {
+                matrixN[numN - (loop + 1), col] = counter;
+                counter++;
+            }
+            for (int row = numN - (loop + 2); row >= (loop + 1); row--)
+            {
+                matrixN[row, loop] = counter;
+                counter++;
+            }
+        }
+
+        for (int row = 0; row < numN; row++) // print result
+        {
+            for (int col = 0; col < numN; col++)
+            {
+                Console.Write("{0, 4}", matrixN[row, col]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
