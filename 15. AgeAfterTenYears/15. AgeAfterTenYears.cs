3f4199c2ab19b8311f7081c605ef2812b102bd7c
stuckata <stu.kirkov@gmail.com>
2014-06-08 00:00:53 +0300

    initial

---
+++ 15. AgeAfterTenYears/15. AgeAfterTenYears.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class AgeAfterTenYears
+{
+    static void Main(string[] args)
+    {
+        int ageAfterTenYears = 0;
+        Console.Write("Please enter your years: ");
+        int age = int.Parse(Console.ReadLine());
+        ageAfterTenYears = age + 10;
+        Console.WriteLine("You are now " + age + " and after 10 years you will be " + ageAfterTenYears + " years old.");
+    }
+}
---
+++ 16. PrintLongSequence/16. PrintLongSequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class PrintLongSequence
+{
+    static void Main()
+    {
+        int firstNumber = 2;
+        int multiplier = 1;
+
+        for (int i = firstNumber; i < firstNumber + 1000; i++)
+        {
+            Console.Write(i * multiplier);
+            if (i != firstNumber + 1000 - 1)
+            {
+                Console.Write(", ");
+            }
+            multiplier = multiplier * (-1);
+        }
+
+    }
+}
